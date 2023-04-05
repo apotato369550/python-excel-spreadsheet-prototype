7303f3ecd8f00f3782bfcfbfa3ea574db9d1722f
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2023-04-05 22:01:13 +0800

    Added basic headers, labels, and entries for adding an invoice

--- basic_tkinter_arar_maker/main.py
+++ basic_tkinter_arar_maker/main.py
@@ -18,13 +18,42 @@ class ARARMaker(tk.Tk):
         self.items = []
 
         self.make_user_interface()
-
         self.mainloop()
 
         return
 
     def make_user_interface(self):
-        return
+        self.user_interface_frame = Frame(self, width=self.max_width, height=self.max_height)
+        self.user_interface_frame.pack()
+
+        self.title_label = Label(self.user_interface_frame, text="ARAR Maker", font=("Courier", 30, "bold"))
+        self.title_label.grid(row=0, column=0, pady=5, columnspan=2)
+        
+        self.add_invoice_label = Label(self.user_interface_frame, text="Add an invoice", font=("Arial", 20, "bold"))
+        self.add_invoice_label.grid(row=1, column=0, pady=5, columnspan=2)
+
+        self.invoice_number_label_1 = Label(self.user_interface_frame, text="Invoice #")
+        self.invoice_number_label_1.grid(row=2, column=0, pady=5)
+        self.invoice_number_entry_1 = Entry(self.user_interface_frame, width=30, borderwidth=5)
+        self.invoice_number_entry_1.grid(row=3, column=0)
+
+        self.name_label_1 = Label(self.user_interface_frame, text="Customer Name")
+        self.name_label_1.grid(row=2, column=1, pady=5)
+        self.name_entry_1 = Entry(self.user_interface_frame, width=30, borderwidth=5)
+        self.name_entry_1.grid(row=3, column=1)
+
+        self.date_label_1 = Label(self.user_interface_frame, text="Due Date (mm/dd/yyyy)",)
+        self.date_label_1.grid(row=2, column=2, pady=5)
+        self.date_entry_1 = Entry(self.user_interface_frame, width=30, borderwidth=5)
+        self.date_entry_1.grid(row=3, column=2)
+
+        self.amount_label_1 = Label(self.user_interface_frame, text="Amount",)
+        self.amount_label_1.grid(row=2, column=3, pady=5)
+        self.amount_entry_1 = Entry(self.user_interface_frame, width=30, borderwidth=5)
+        self.amount_entry_1.grid(row=3, column=3)
+
+        self.delete_invoice_label = Label(self.user_interface_frame, text="Delete an Invoice", font=("Arial", 20, "bold"))
+        self.delete_invoice_label.grid(row=7, column=0, pady=5, columnspan=2)
 
 
 if __name__ == "__main__":
