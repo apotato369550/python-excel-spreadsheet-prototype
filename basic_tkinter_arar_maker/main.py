328d526d133221584e08cd28136778e18e9652b1
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2023-04-10 23:14:59 +0800

    Added all necessary widgets to interface

--- basic_tkinter_arar_maker/main.py
+++ basic_tkinter_arar_maker/main.py
@@ -27,8 +27,10 @@ class ARARMaker(tk.Tk):
         self.user_interface_frame.pack()
 
         self.title_label = Label(self.user_interface_frame, text="ARAR Maker", font=("Courier", 30, "bold"))
-        self.title_label.grid(row=0, column=0, pady=5, columnspan=2)
+        self.title_label.grid(row=0, column=1, pady=5, columnspan=2)
         
+        # add invoice button
+
         self.add_invoice_label = Label(self.user_interface_frame, text="Add an invoice", font=("Arial", 20, "bold"))
         self.add_invoice_label.grid(row=1, column=0, pady=5, columnspan=2)
 
@@ -52,8 +54,49 @@ class ARARMaker(tk.Tk):
         self.amount_entry_1 = Entry(self.user_interface_frame, width=30, borderwidth=5)
         self.amount_entry_1.grid(row=3, column=3)
 
+        # put command here
+        self.add_invoice_button = Button(self.user_interface_frame, text="Add Invoice", font=("Courier", 15, "bold"))
+        self.add_invoice_button.grid(row=4, column=1, columnspan=2)
+
+        # delete invoice feature
+
         self.delete_invoice_label = Label(self.user_interface_frame, text="Delete an Invoice", font=("Arial", 20, "bold"))
-        self.delete_invoice_label.grid(row=7, column=0, pady=5, columnspan=2)
+        self.delete_invoice_label.grid(row=5, column=0, pady=5, columnspan=2)
+        
+        self.invoice_number_label_2 = Label(self.user_interface_frame, text="Invoice #")
+        self.invoice_number_label_2.grid(row=6, column=0, pady=5)
+        self.invoice_number_entry_2 = Entry(self.user_interface_frame, width=30, borderwidth=5)
+        self.invoice_number_entry_2.grid(row=7, column=0)
+
+        self.name_label_2 = Label(self.user_interface_frame, text="Customer Name (Optional)")
+        self.name_label_2.grid(row=6, column=1, pady=5)
+        self.name_entry_2 = Entry(self.user_interface_frame, width=30, borderwidth=5)
+        self.name_entry_2.grid(row=7, column=1)
+
+        self.date_label_2 = Label(self.user_interface_frame, text="Due Date (mm/dd/yyyy) (Optional)",)
+        self.date_label_2.grid(row=6, column=2, pady=5)
+        self.date_entry_2 = Entry(self.user_interface_frame, width=30, borderwidth=5)
+        self.date_entry_2.grid(row=7, column=2)
+
+        self.amount_label_2 = Label(self.user_interface_frame, text="Amount (Optional)",)
+        self.amount_label_2.grid(row=6, column=3, pady=5)
+        self.amount_entry_2 = Entry(self.user_interface_frame, width=30, borderwidth=5)
+        self.amount_entry_2.grid(row=7, column=3)
+
+        self.add_invoice_button = Button(self.user_interface_frame, text="Delete Invoice", font=("Courier", 15, "bold"))
+        self.add_invoice_button.grid(row=8, column=1, columnspan=2)
+
+        # listbox and view invoices button
+        
+        self.item_listbox_label = Label(self.user_interface_frame, text="AR Aging Report Overview", font=("Arial", 13))
+        self.item_listbox_label.grid(row=9, column=0, columnspan=2)
+
+        self.item_listbox = Listbox(self.user_interface_frame, width=40, height=12, selectmode="SINGLE", borderwidth=5)
+        self.item_listbox.grid(row=10, column=0, columnspan=2)
+
+        self.add_invoice_button = Button(self.user_interface_frame, text="View AR Aging Report", font=("Courier", 15, "bold"))
+        self.add_invoice_button.grid(row=9, column=2, rowspan=2)
+
 
 
 if __name__ == "__main__":
